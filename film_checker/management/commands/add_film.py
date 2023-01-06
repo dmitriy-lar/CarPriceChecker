5beeb0314de0610001c1659e592060e1f3a4e8cc
dmitriy-lar <larionov.dm2002@gmail.com>
2023-01-06 20:11:08 +0300

    Add command 'add_film.py'

--- film_checker/management/commands/add_film.py
+++ film_checker/management/commands/add_film.py
@@ -1,11 +1,17 @@
 from django.core.management.base import BaseCommand, CommandError
+from film_checker.models import Film
 
 
 class Command(BaseCommand):
-    help = 'Print hello with argument __name__'
+    help = 'Add film in the database'
 
     def add_arguments(self, parser):
-        parser.add_argument('name', type=str)
+        parser.add_argument('film_name', type=str)
 
     def handle(self, *args, **options):
-        self.stdout.write(self.style.SUCCESS('Hello %s' % options['name']))
+        if Film.objects.filter(title=options['film_name']).exists():
+            raise CommandError('Film already exists. Try another film')
+        Film(title=options['film_name']).save()
+        self.stdout.write(self.style.SUCCESS('Film [%s] was successfully added' % options['film_name']))
+
+
--- film_checker/models.py
+++ film_checker/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 
 class Film(models.Model):
-    title = models.CharField(max_length=256, null=False, blank=False)
+    title = models.CharField(max_length=256, null=False, blank=False, unique=True)
     last_rating = models.CharField(max_length=6, null=True, blank=True)
-    last_time_checked = models.DateTimeField()
+    last_time_checked = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
         return self.title
