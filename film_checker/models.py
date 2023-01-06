741868c9dea3dbc733c697a3819f3d90d580232b
dmitriy-lar <larionov.dm2002@gmail.com>
2023-01-06 20:00:58 +0300

    Add Film Model

--- film_checker/models.py
+++ film_checker/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Film(models.Model):
+    title = models.CharField(max_length=256, null=False, blank=False)
+    last_rating = models.CharField(max_length=6, null=True, blank=True)
+    last_time_checked = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
